defc7c04d5112951c7bca7a2ccd0f73bcf6d7950
Hannah Fenlon <hgf2108@columbia.edu>
2019-01-17 10:42:38 -0500

    committing changes

---
+++ Website/js/index.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- All your head attributes -->
+    </head>
+
+<body>
+
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+      <script src="/js/index.js"></script>
+
+
+</body>
